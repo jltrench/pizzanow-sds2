43c248d6d0165a3eed9ef7173240ea82e3587712
João Lucca Trench <jltrench@gmail.com>
2021-09-22 19:57:15 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/pizzanow/PizzanowApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.pizzanow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzanowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzanowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/pizzanow/PizzanowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.pizzanow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzanowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
